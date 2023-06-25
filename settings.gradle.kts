27594e2c8a90f9ae05056ede6496eee541070dc3
墨水瓶Official <im@inker.bot>
2023-06-25 18:27:57 +0800

    [ci skip] Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "Terra"
 
 fun includeImmediateChildren(dir: File, type: String) {
     dir.walkTopDown().maxDepth(1).forEach {
-        if (!it.isDirectory || !File(it, "build.gradle.kts").exists()) return@forEach
+        if (!it.isDirectory || !File(it, "build.gradle.kts").exists() || File(it, "disable").exists()) return@forEach
         val addonDir = it.relativeTo(file(".")).path.replace("/", ":").replace("\\", ":")
         println("Including $type directory \"$addonDir\" as subproject.")
         include(addonDir)
